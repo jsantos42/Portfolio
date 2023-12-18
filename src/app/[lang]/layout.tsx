71250b4db46540d61270f7ec02b827850182968d
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-12-18 16:59:58 +0000

    fix: fix 'Referenced UMD global variable' warning

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../globals.css';
 import { PageParams } from '@/types';
+import React from "react";
 
 const inter = Inter({ subsets: ['latin'] });
 
