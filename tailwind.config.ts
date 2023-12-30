008305fe5d3f3dd1610bd6c8ab65f7ebfe331b5a
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-12-30 11:43:58 +0000

    feat: add global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss';
 
+// Using rgba() instead of rgb() to allow the use of the opacity modifier
+// cf https://tailwindcss.com/docs/background-color#changing-the-opacity
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundColor: {
+				theme: 'rgba(var(--background-rgb))',
+			},
+			colors: {
+				theme: 'rgba(var(--foreground-rgb))',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
