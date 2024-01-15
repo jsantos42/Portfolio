38d548dbcd895ffb89795b19ce5e3ea145955830
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-01-15 23:26:34 +0100

    feat: add homepage styles for desktop, and improve SEO

--- src/app/[lang]/[slug]/page.tsx
+++ src/app/[lang]/[slug]/page.tsx
@@ -9,39 +9,49 @@ export default function Home({ params }: { params: PageParams }) {
 	const infoEntries = Object.entries(data.info) as [InfoType, string][];
 
 	return (
-		<>
-			<div className="p-6 antialiased tracking-tight">
-				<h2 className="text-3xl text-primary font-semibold mb-1">
-					{data.name}
-				</h2>
-				{(pageContent.headline as string).split('|').map((i, index) => (
-					<h2 key={index} className="font-semibold">
-						{i}
-					</h2>
-				))}
-			</div>
-			<div className="flex justify-start pl-[14px] pb-24">
-				{infoEntries
-					.filter(
-						([key, value]) =>
-							key === 'emailAddress' ||
-							key === 'linkedInUser' ||
-							key === 'gitHubUser'
-					)
-					.sort(([keyA, valueA], [keyB, valueB]) =>
-						keyA < keyB ? 1 : -1
-					)
-					.map(([key, value]) => (
-						<TouchableLink
-							key={key}
-							href={getInfoLink(key, value)}
-							inNewWindow={true}
-						>
-							{getInfoSvg(key)}
-						</TouchableLink>
-					))}
-			</div>
-			<div className="px-6 pb-16 leading-relaxed antialiased">
+		<div
+			className="sm:w-[500px] lg:w-auto lg:max-w-screen-xl mx-auto p-6
+		lg:pt-16 lg:px-24 flex flex-wrap justify-start"
+		>
+			<header className="min-w-[232px] min-[345px]:min-w-[305px] lg:w-1/2">
+				<div className="antialiased tracking-tight">
+					<h1 className="text-3xl lg:text-4xl text-primary font-semibold mb-1">
+						{data.name}
+					</h1>
+					{(pageContent.headline as string)
+						.split('|')
+						.map((i, index) => (
+							<h2
+								key={index}
+								className="text-lg lg:text-xl font-semibold"
+							>
+								{i}
+							</h2>
+						))}
+				</div>
+				<div className="flex justify-start pt-6 pb-24 -ml-[9px]">
+					{infoEntries
+						.filter(
+							([key, value]) =>
+								key === 'emailAddress' ||
+								key === 'linkedInUser' ||
+								key === 'gitHubUser'
+						)
+						.sort(([keyA, valueA], [keyB, valueB]) =>
+							keyA < keyB ? 1 : -1
+						)
+						.map(([key, value]) => (
+							<TouchableLink
+								key={key}
+								href={getInfoLink(key, value)}
+								inNewWindow={true}
+							>
+								{getInfoSvg(key)}
+							</TouchableLink>
+						))}
+				</div>
+			</header>
+			<main className="lg:w-1/2 max-w-[500px] pb-16 leading-relaxed antialiased">
 				<Markdown
 					className={'flex flex-col gap-y-3'}
 					components={{
@@ -52,7 +62,7 @@ export default function Home({ params }: { params: PageParams }) {
 				>
 					{pageContent.summary}
 				</Markdown>
-			</div>
-		</>
+			</main>
+		</div>
 	);
 }
