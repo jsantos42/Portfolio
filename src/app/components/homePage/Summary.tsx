41c3f005f91409d00c5e995a7086e002e412d501
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-02-11 21:55:06 +0100

    feat: improve readability of summary

--- src/app/components/homePage/Summary.tsx
+++ src/app/components/homePage/Summary.tsx
@@ -4,7 +4,7 @@ export const Summary = ({ text }: { text: string }) => (
 	<Markdown
 		className={'flex flex-col gap-y-3'}
 		components={{
-			strong: props => <strong className="text-primary" {...props} />,
+			strong: props => <strong className="font-semibold" {...props} />,
 		}}
 	>
 		{text}
